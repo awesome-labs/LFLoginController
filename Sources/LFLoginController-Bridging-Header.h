c1e40d4f4a88479f5a993c8a7baa45d33622dbfd
João Mourato <jmourato@blissapplications.com>
2016-08-03 12:04:50 +0100

    Carthage compatibility

---
+++ Sources/LFLoginController-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  LFLoginController-Bridging-Header.h
+//  LFLoginController
+//
+//  Created by João Mourato on 03/08/16.
+//  Copyright © 2016 Lucas Farah. All rights reserved.
+//
+
+#ifndef LFLoginController_Bridging_Header_h
+#define LFLoginController_Bridging_Header_h
+
+#import "OnePasswordExtension.h"
+
+#endif /* LFLoginController_Bridging_Header_h */
---
+++ Sources/LFLoginControllerFramework.h
@@ -0,0 +1,19 @@
+//
+//  LFLoginControllerFramework.h
+//  LLFLoginControllerFramework
+//
+//  Created by JARMourato on 3/8/16.
+//  Copyright © 2016 Lucas Farah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LFLoginControllerFramework.
+FOUNDATION_EXPORT double LFLoginControllerFrameworkVersionNumber;
+
+//! Project version string for LFLoginControllerFramework.
+FOUNDATION_EXPORT const unsigned char LFLoginControllerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LFLoginControllerFramework/PublicHeader.h>
+
+
