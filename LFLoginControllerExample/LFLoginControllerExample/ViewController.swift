15d965fbba9e444a9b282d2f6a0cdc02213ba435
Lucas Farah <lucas.farah@me.com>
2016-09-06 02:02:37 -0700

    Updated version

--- LFLoginControllerExample/LFLoginControllerExample/ViewController.swift
+++ LFLoginControllerExample/LFLoginControllerExample/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         controller.delegate = self
         
         // Customizations
